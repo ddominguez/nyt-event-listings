ad65ac1fe7cc1b3a1f8f12e5d54350b7cf6bf26d
David Dominguez <ddominguez@gmail.com>
2014-10-21 18:24:13 -0400

    added uri and params

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 DEBUG = False
 
 api_key = ''
@@ -7,3 +9,14 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+today = date.today().isoformat()
+api_params = {
+    'api-key': api_key,
+    'limit': 50,
+    'filters': 'category:(-Movies)',
+    'sort': 'last_chance desc',
+    'date_range': '%s:%s' % (today, today)
+}
+
+api_uri = 'http://api.nytimes.com/svc/events/%s/listings' % api_version
