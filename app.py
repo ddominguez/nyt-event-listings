a1e5f907044ec31f3f9735694b31340964ea7e34
David Dominguez <ddominguez@gmail.com>
2014-10-21 18:23:44 -0400

    get events

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+import requests
+import settings
+from flask import Flask, render_template
+
 app = Flask(__name__)
+app.config['DEBUG'] = settings.DEBUG
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    r = requests.get(settings.api_uri, params=settings.api_params)
+    data = r.json()
+    return render_template(
+        'index.html',
+        events=data['results']
+    )
+
 
 if __name__ == "__main__":
     app.run()
