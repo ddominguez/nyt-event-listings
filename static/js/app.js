cf5a826419669fafba229240450e899dc59df9a7
David Dominguez <ddominguez@gmail.com>
2014-10-23 21:38:49 -0400

    added bouncy markers to map

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,37 @@
+var events_length = events.length;
+var iterator = 0;
+var map;
+
 function initialize() {
     var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: parseFloat(events[0]['geocode_latitude']), lng: parseFloat(events[0]['geocode_longitude'])},
+        zoom: 12,
     };
-    var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
+    var i;
+    for (i=0; i<events_length; i++) {
+        setTimeout(function(){
+            addMarker();
+        }, i*200);
+    }
+}
+
+function addMarker() {
+    var marker = new google.maps.Marker({
+        position: {lat: parseFloat(events[iterator]['geocode_latitude']), lng: parseFloat(events[iterator]['geocode_longitude'])},
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+    var infowindow = new google.maps.InfoWindow();
+    var contentHtml;
+    google.maps.event.addListener(marker, 'click', (function(marker, iterator) {
+        return function() {
+            contentHtml = '<strong>'+events[iterator]['event_name']+'</strong>';
+            infowindow.setContent(contentHtml);
+            infowindow.open(map, marker);
+        }
+    })(marker, iterator));
+    iterator++;
 }
 google.maps.event.addDomListener(window, 'load', initialize);
